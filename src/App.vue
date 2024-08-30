209bd75f686db60302bfcc7c0d3e3169b3159a19
Juan Carlos Quintero <juanquinterodj@gmail.com>
2024-08-30 17:44:11 -0300

    Installed and configured tailwind

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 </script>
 
 <template>
-  <div></div>
+  <div><h1 class="text-3xl font-bold underline">
+      Hello, Tailwind CSS with Vue 3!
+    </h1></div>
 </template>
 
 <style scoped></style>
