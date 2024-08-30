d4a749a009805cdfb4787b3122d936c127a1f194
Juan Carlos Quintero <juanquinterodj@gmail.com>
2024-08-30 20:42:05 -0300

    Added colors used in Figma to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = {
+  accent: "#F22539",
+  "accent-pressed": "#C00E20",
+  primary: "#353535",
+  secondary: "#5E5E5E",
+  favourite: "#ECA539",
+  white: "#ffffff",
+  light: "#F9F9F9",
+  disable: "#BFBFBF",
+  transparent: "transparent",
+  current: "currentColor",
+};
+
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    colors,
     extend: {},
   },
   plugins: [],
-}
-
+};
